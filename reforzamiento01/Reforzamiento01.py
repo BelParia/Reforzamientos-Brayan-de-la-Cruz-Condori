6b48e3d17ff550ebf72019bcddfcfa40efefcc81
BelParia <brayandelacon@gmail.com>
2025-09-20 12:50:19 -0500

    test gitignore

---
+++ reforzamiento01/Reforzamiento01.py
@@ -0,0 +1,22 @@
+# REFORZAMIENTO 1
+"""
+1. Crear un proyecto con tu nombre y un apellido, por ejemplo: “luis_gamarra_rf01”
+2. Crear 3 entornos virtuales para este proyecto con diferentes nombres y con la
+siguiente estructura de nombres (reemplazarlo por tu apellido):
+“env_001_gamarra”, “env_002_gamarra”, etc
+3. Cada entorno virtual tendrá instaladas diferentes librerías.
+4. Obtener todos los archivos requeriments.txt de cada entorno virtual
+5. Para el primer entorno virtual instalar las librerías: ironpdf, numpy, seaborn
+6. Para el segundo entorno virtual instalar las librerías: matplotlib, pillow, qrcode
+7. Para el tercer entorno virtual instalar las librerías: pandas, opencv-python, pymysql
+8. Para el cuarto entorno virtual instalar las librerías: django, spark, awscli
+9. Instalar alguna librería adicional vista en clase a tus entornos virtuales creados,
+indicar cuál fue
+10. Finalmente presentar las pantallas (screemshots) necesarias de cada uno de estos
+entornos virtuales y el contenido de sus archivos requirements.txt: total 4 screemshot
+como se ve en el siguiente ejemplo:
+
+Sé que aquí no esta directamente los entornos, ya que esto fue realizado con tomas de
+capturas y enviados en un pdf, por eso es que como tal no hay una actividad de comandos.
+
+"""
