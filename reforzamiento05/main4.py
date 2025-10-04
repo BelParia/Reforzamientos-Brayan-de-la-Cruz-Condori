a1ee95a51ab4f160a97869771de92a4bbb3271f2
BelParia <brayandelacon@gmail.com>
2025-10-04 15:37:36 -0500

    feat(reforzamiento05) agrego ejercicio 4 de Modulo del reformzamiento

--- reforzamiento05/main4.py
+++ reforzamiento05/main4.py
@@ -4,7 +4,7 @@
 
 ### Ejercicio 4
 """
-Creando un archivo principal (main.py) donde importará a un módulo
+Creando un archivo principal (main4.py) donde importará a un módulo
 (operaciones.py) el cuál contendrá las siguientes funciones: - Una función que genere 30 números enteros aleatorios entre 0 y
 100, y muestre en pantalla esta lista de números aleatorios - Otra función que ordene los valores de una lista y volver a mostrarla
 en pantalla - Otra función que me indicará cuál es el mayor de todos estos
