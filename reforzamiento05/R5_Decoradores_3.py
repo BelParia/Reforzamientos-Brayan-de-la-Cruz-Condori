ecaa5fa3f8fd420fdec08defdee1eb05c9e4db86
BelParia <brayandelacon@gmail.com>
2025-10-04 16:24:25 -0500

    feat(reforzamiento05) agrego el ejercicio 3 de Decoradores del reforzamiento 5

---
+++ reforzamiento05/R5_Decoradores_3.py
@@ -0,0 +1,25 @@
+# Reforzamiento 5
+
+## Modulo
+
+### Ejercicio 3
+"""
+Crear un decorador conteo_parametros(funcion) donde imprimirá la
+cantidad de argumentos que tiene la función a decorar usando *args y
+**kwargs.
+Mensaje previo “La cantidad de argumentos que tiene la función es”
+mensaje post “La función decoradora terminó de ejecutarse
+correctamente”
+Ejemplo: Al usar una función suma que creamos:
+suma(4, 1, 10, 2, 50, 64)
+Salida:
+“La cantidad de argumentos que tiene la función es 5”
+“La función decoradora terminó de ejecutarse correctamente”
+Consideración:
+Todos los parámetros ingresados deben ser enteros, caso sean String
+alguno de ellos indicar al usuario: “Solo está admitido datos enteros,
+no se podrá realizar la suma”
+Usar la función al menos 3 veces
+"""
+
+## Como se habrá dado cuenta, no pude terminar todos los ejercicios.
